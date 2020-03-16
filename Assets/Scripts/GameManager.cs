37ae5a7207b45a9f6379bb356820637e2133e62f
Han <bpaucarm@outlook.com>
2020-03-16 11:41:06 -0500

    Desde la pc de Brus - terminado el tuto 3

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager1 : MonoBehaviour{
+public class GameManager : MonoBehaviour{
 
     public BoardManager boardScript;
 

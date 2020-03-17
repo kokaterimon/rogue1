7fe192d5985f4fe90b3e7101b4e77c60bbe2a6f6
kokaterimon <bpaucarm@outlook.com>
2020-03-17 14:24:44 -0500

    terminando el tutorial 7

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingObject : MonoBehaviour{
+public abstract class MovingObject : MonoBehaviour{
 
     public float moveTime = 0.1f;
     public LayerMask blokingLayer;
@@ -10,6 +10,7 @@ public class MovingObject : MonoBehaviour{
     private float movemetSpeed;
     private BoxCollider2D boxCollider;
     private Rigidbody2D rb2D;
+    private Vector2 newPosition;
 
     private void Awake()
     {
@@ -22,12 +23,42 @@ public class MovingObject : MonoBehaviour{
         movemetSpeed = 1 / moveTime;
     }
 
+    protected IEnumerator SmoothMovemet(Vector2 end)
+    {
+        float remainigDistance = Vector2.Distance(rb2D.position, end);
+        while (remainigDistance > float.Epsilon)
+        {
+            Vector2 newPOsition = Vector2.MoveTowards(rb2D.position, end, movemetSpeed * Time.deltaTime);
+            rb2D.MovePosition(newPosition);
+            remainigDistance = Vector2.Distance(rb2D.position, end);
+            yield return null;
+        }
+    }
+
     protected bool Move(int xDir, int yDir, out RaycastHit2D hit)
     {
         Vector2 start = transform.position;
         Vector2 end = start + new Vector2(xDir, yDir);
         boxCollider.enabled = false;
-        hit = Physics2D.Linecast(start, end, blokingLayer)
+        hit = Physics2D.Linecast(start, end, blokingLayer);
         boxCollider.enabled = true;
+        if(hit.transform == null)
+        {
+            StartCoroutine(SmoothMovemet(end));
+            return true;
+        }
+        return false;
     }
+
+    protected abstract void OnCantMove(GameObject go);
+
+    protected virtual void AttemptMove(int xDir, int yDir)
+    {
+        RaycastHit2D hit;
+        bool canMove = Move(xDir, yDir, out hit);
+        if (canMove) return;
+
+        OnCantMove(hit.transform.gameObject);
+    }
+
 }

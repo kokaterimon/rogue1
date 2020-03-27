557727be8fcd3ec854458b8efe0247d280a79333
kokaterimon <bpaucarm@gmail.com>
2020-03-27 09:32:57 -0500

    Arreglado por Br

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,28 +1,17 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MovingObject {
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class Enemy : MovingObject{
->>>>>>> 9ae4b4c3eeb9e5c92dcfe4dcf9b5400f51a7783d
+
 
     public int playerDamage;
 
     private Animator animator;
     private Transform target;
     private bool skipMove;
-<<<<<<< HEAD
 
-=======
-           
->>>>>>> 9ae4b4c3eeb9e5c92dcfe4dcf9b5400f51a7783d
     protected override void Awake()
     {
         animator = GetComponent<Animator>();
@@ -46,35 +35,29 @@ public class Enemy : MovingObject{
         skipMove = true;
     }
 
-<<<<<<< HEAD
     public void MoveEnemy()
     {
         int xDir, yDir;
-        if(Math.Abs(target.position.x - transform.position.x) < float.Epsilon)
+        if (Math.Abs(target.position.x - transform.position.x) < float.Epsilon)
         {
             yDir = target.position.y > transform.position.y ? 1 : -1;
         }
-=======
+    }
+
     public void moveEnemy()
     {
         int xDir, yDir;
         //if()
->>>>>>> 9ae4b4c3eeb9e5c92dcfe4dcf9b5400f51a7783d
+
     }
 
     protected override void OnCantMove(GameObject go)
     {
-<<<<<<< HEAD
-        //FALTA CÒDIGO AQUÌ
+
     }
 
 
 
-}       
-=======
-        //FALTA CÓDIGO AQUÍ!!!!!
-    }
+}      
 
-
-}
->>>>>>> 9ae4b4c3eeb9e5c92dcfe4dcf9b5400f51a7783d
+    
